4692f903190e67a8d8bab8039d21b7770d2b4570
rkhamis <khamisr@codescalers.com>
2014-09-10 16:01:33 +0300

    adding libs

--- prototypes/confluence2rst/confluence2rst.py
+++ prototypes/confluence2rst/confluence2rst.py
@@ -5,12 +5,12 @@ import JumpScale.portal.docgenerator
 import lib.objectinspector
 
 
-# ignore = ["zredisgw"]
-# dest = "/opt/code/github/jumpscale/generated_docs/docs/source/API/"
-# j.tools.objectinspector.generateDocs(dest, ignore)
+ignore=["zredisgw", "ipshell"]
+dest="/opt/code/github/jumpscale/jumpscale_docs/Spaces/Doc_Jumpscale_Libraries/Docs/"
+j.tools.objectinspector.generateDocs(dest,ignore)
 
 src = "/opt/code/github/jumpscale/jumpscale_docs/Spaces/"
-dst = "/opt/code/github/jumpscale/generated_docs/docs/source/spaces"
+dst = "/opt/code/github/jumpscale/generated_docs/docs/_source/spaces"
 j.tools.docgenerator.convertConfluenceToRST(src, dst)
 
 j.application.stop()
